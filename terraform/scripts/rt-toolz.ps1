c82511527c48596b9d2751548f9a10aae0389480
q0phi80 <lab.pwc@gmail.com>
2022-08-09 15:38:11 -0400

    Few updates

--- terraform/scripts/rt-toolz.ps1
+++ terraform/scripts/rt-toolz.ps1
@@ -6,3 +6,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 # Install some toolz
 choco install git googlechrome processhacker python burp-suite-free-edition zap autopsy 7zip adobereader adexplorer apimonitor apktool netfx-4.8 cutter dnspy ghidra golang ida-free javadecompiler-gui firefox wget -y
+
+# Download more toolz
+Set-Location -Path "C:\toolz"
+Invoke-WebRequest -Uri https://github.com/BloodHoundAD/BloodHound/releases/download/4.2.0/BloodHound-win32-x64.zip -OutFile BloodHound.zip
